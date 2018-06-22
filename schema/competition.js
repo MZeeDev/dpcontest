df1fae680d7bf3f72e802dee7cc7f7f5d8070cc1
MZeeDev <ziaullah623@gmail.com>
2018-06-22 22:31:56 +0500

    ok

--- schema/competition.js
+++ schema/competition.js
@@ -2,8 +2,10 @@ const { mongoose } = require('./mongoose')
 var CompetitionSchema = new mongoose.Schema(
     {
         userID:mongoose.Types.ObjectId,
-        candidate1: String,
-        candidate2: String,
+        candidate1: mongoose.Types.ObjectId,
+        candidate2: mongoose.Types.ObjectId,
+        // candidate1: String,
+        // candidate2: String,
         createdAt:timestamps,
         hours: {
             type: timestamps
